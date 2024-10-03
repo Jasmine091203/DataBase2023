d3757b7ba0f7edcdee982e9dc87b8e718da245aa
Wong, Tien-Yu <149470955+Jasmine091203@users.noreply.github.com>
2024-10-04 02:14:41 +0800

    Add files via upload

---
+++ AirplanesAPI-main/Airplanes/Dtos/AirplaneOfAirport.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Airplanes.Dtos
+{
+    public class AirplaneOfAirport
+    {
+        [Required]
+        public Guid Aid { get; set; }
+        [Required]
+        public string Aname { get; set; }
+        public List<String> Airplanes { get; set; } = new List<String>();
+    }
+}
---
+++ AirplanesAPI-main/Airplanes/Dtos/AirportOfAirplane.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Airplanes.Dtos
+{
+    public class AirportOfAirplane
+    {
+        [Required]
+        public Guid Pid { get; set; }
+        [Required]
+        public string Pname { get; set; }
+        public List<String> Airports { get; set; } = new List<String>();
+    }
+}
