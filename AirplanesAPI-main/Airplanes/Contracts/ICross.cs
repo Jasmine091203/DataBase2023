33f5b39715b381f9b5c9987a3ce3989119d8d998
Wong, Tien-Yu <149470955+Jasmine091203@users.noreply.github.com>
2024-10-04 02:17:07 +0800

    Add files via upload

---
+++ AirplanesAPI-main/Airplanes/Contracts/ICross.cs
@@ -0,0 +1,13 @@
+﻿using Airplanes.Dtos;
+
+namespace Airplanes.Contracts
+{
+    public interface ICross
+    {
+        //P04第10頁 
+        public Task<AirplaneOfAirport> GetAirplanesByAirportId(Guid id);
+        public Task<AirportOfAirplane> GetAirportsByAirplaneId(Guid id);
+        public Task<AirplaneDetailsOfAirport> GetAirplaneDetailsByAirportId(Guid id);
+        public Task<AirportDetailsOfAirplane> GetAirportDetailsByAirplaneId(Guid id);
+    }
+}
